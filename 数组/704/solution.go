96f214750c48e68a7f41eaab850a339f48f37129
qianhao-zjut <1771491417@qq.com>
2023-10-21 20:36:54 +0800

    update

--- 数组/704/solution.go
+++ 数组/704/solution.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+//704.二分查找
+func search(nums []int, target int) int {
+	begin := 0
+	end := len(nums)
+	for begin < end {
+		mid := (begin + end) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			begin = mid + 1
+		} else {
+			end = mid
+		}
+	}
+	return -1
+}
+
+
+
+
+
+
 func main() {
-	fmt.Println("Hello")
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	fmt.Println(search(nums, -1))
 }
