1ea05d965d0f994365c2433c66893cd052ab1f31
qianhao-zjut <1771491417@qq.com>
2023-10-22 16:01:14 +0800

    update

---
+++ 数组/977/solution.go
@@ -0,0 +1,25 @@
+package main
+
+
+
+func sortedSquares(nums []int) []int {
+	maxIndex := len(nums) - 1
+	minIndex := 0
+	res := make([]int, 0,len(nums))
+	res_index := len(nums) - 1
+	for minIndex <= maxIndex {
+		if square(nums[maxIndex]) > square(nums[minIndex]) {
+			res[res_index] = square(nums[maxIndex])
+			maxIndex--
+		} else {
+			res[res_index] = square(nums[minIndex])
+			minIndex++
+		}
+		res_index--
+	}
+	return res
+}
+
+func square(x int) int {
+    return x * x
+}
