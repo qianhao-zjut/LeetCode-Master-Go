21c039772d2337db3a3dc164730c1ed6f238af57
haoqian <haoqian@futunn.com>
2023-10-24 10:28:04 +0800

    update

--- 数组/977/solution.go
+++ 数组/977/solution.go
@@ -3,11 +3,15 @@ package main
 
 
 func sortedSquares(nums []int) []int {
+
 	maxIndex := len(nums) - 1
 	minIndex := 0
+
 	res := make([]int, 0,len(nums))
 	res_index := len(nums) - 1
+
 	for minIndex <= maxIndex {
+
 		if square(nums[maxIndex]) > square(nums[minIndex]) {
 			res[res_index] = square(nums[maxIndex])
 			maxIndex--
@@ -15,8 +19,10 @@ func sortedSquares(nums []int) []int {
 			res[res_index] = square(nums[minIndex])
 			minIndex++
 		}
+
 		res_index--
 	}
+	
 	return res
 }
 
