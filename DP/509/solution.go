75a961613d57606755aa801af6f32daa9fe43c3a
haoqian <haoqian@futunn.com>
2023-10-26 15:04:06 +0800

    update

---
+++ DP/509/solution.go
@@ -0,0 +1,19 @@
+package main
+
+
+func fib(n int) int {
+
+	if n == 0 || n == 1 {
+		return n
+	}
+
+	dp := make([]int, n+1,n+1)
+	dp[0] = 0
+	dp[1] = 1
+
+	for i := 2; i < n+1; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
